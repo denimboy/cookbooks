085311e026a968189d182b52872c2c5b4dd7431e
Dennis Watson <dennis@xprdev.com>
2010-10-05 15:32:16 -0700

    Dont make ree the default ruby here.

--- rvm/recipes/ree.rb
+++ rvm/recipes/ree.rb
@@ -1,16 +1,11 @@
 #
-# Installs REE using RVM.  Makes REE default ruby
+# Installs ree using rvm.
 #
 
 require_recipe "rvm"
 
-bash "install REE in RVM" do
+bash "Install ree in rvm" do
   user "root"
   code "rvm install ree"
   not_if "rvm list | grep ree"
-end
-
-bash "make REE the default ruby" do
-  user "root"
-  code "rvm --default ree"
 end
